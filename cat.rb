a31edbcc3e37ac9e3eced5891d8cca3df604a615
ChrisP <600steps@gmail.com>
2016-08-10 09:46:13 -0400

    comments regarding methods

--- cat.rb
+++ cat.rb
@@ -1,28 +1,31 @@
 class Cat
-   def initialize(name, food, time)
+   def initialize(name, preferred_food, meal_time)
       @name = name
-      @food = food
-      @time = time.to_i
+      @preferred_food = preferred_food
+      @meal_time = meal_time.to_i
    end
 
    def eats_at
-     if @time <= 11
-       puts "eats at #{@time} AM"
+     if @meal_time <= 11
+       puts "eats at #{@meal_time} AM"
      else
-       puts "eats at #{(@time - 12)}  PM" #must -12
+       puts "eats at #{(@meal_time - 12)}  PM"
      end
    end
+  #  def to_s #does this pass @time to a string value
+  #    @time
+  #  end
 
    def meow
-      puts "My name is #{@name}, I eat #{@food}" # must add time
+      puts "My name is #{@name}, I eat #{@preferred_food} "   # must add eats_at method
    end
 end
 
 c1 = Cat.new("Sparkles", "tuna", 7)
 c2 = Cat.new("Fluffy", "salmon", 22)
 
-c1.eats_at
+# c1.eats_at #How do I incorporate this in meow?
 c1.meow
 
-c2.eats_at
+#c2.eats_at #How do I incorporate this in meow?
 c2.meow
