56b6058d4b521ae4b130ec035791b56480bb0ab6
ChrisP <600steps@gmail.com>
2016-08-10 15:13:33 -0400

    minor change to eats_at method and important addition to meow method

--- cat.rb
+++ cat.rb
@@ -1,31 +1,27 @@
 class Cat
-   def initialize(name, preferred_food, meal_time)
+   def initialize(name, preferred_food, meal_time)# declare class and initialize var
       @name = name
       @preferred_food = preferred_food
       @meal_time = meal_time.to_i
    end
 
-   def eats_at
+   def eats_at # method dermines if AM or PM
      if @meal_time <= 11
-       puts "eats at #{@meal_time} AM"
+        " at #{@meal_time} AM"
      else
-       puts "eats at #{(@meal_time - 12)}  PM"
+       " at #{(@meal_time - 12)} PM" #change from 24 hr clock to AM PM format
      end
    end
-  #  def to_s #does this pass @time to a string value
-  #    @time
-  #  end
+
 
    def meow
-      puts "My name is #{@name}, I eat #{@preferred_food} "   # must add eats_at method
+      puts "My name is #{@name}, I eat #{@preferred_food} #{eats_at}" #Note: syntax to display eats_at method
    end
 end
 
-c1 = Cat.new("Sparkles", "tuna", 7)
+c1 = Cat.new("Sparkles", "tuna", 7)#create new instance of class for cat
 c2 = Cat.new("Fluffy", "salmon", 22)
 
-# c1.eats_at #How do I incorporate this in meow?
-c1.meow
 
-#c2.eats_at #How do I incorporate this in meow?
-c2.meow
+c1.meow #calls method for cat 1
+c2.meow#calls method for cat 2
---
+++ paperboy.rb
@@ -0,0 +1,10 @@
+class PaperBoy
+   def initialize(name, experience, side, earnings )
+      @name = name
+      @experience= experience
+      @side = side
+      @earnings = earnings
+   end
+
+   def quotas
+   end
