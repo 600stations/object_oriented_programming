6a9fa3afcd44f5ccc0e5af16809c8f501eb64a27
ChrisP <600steps@gmail.com>
2016-08-11 16:57:34 -0400

    added questions regarding size of Mars, current location and commands

--- mars_rover.rb
+++ mars_rover.rb
@@ -25,11 +25,31 @@ def left_turn(e_direction)#Not sure how to input
   end
   #
   # method to move rover-pseudo code
-  def move(??) # pass in direction?
+  def move(e_direction) # pass in direction?
     case
       when "N" then @y_coordinate = @y_coordinate + 1
       when "E" then @x_coordinate = @x_coordinate + 1
       when "W" then @x_coordinate = @x_coordinate - 1
       when "S" then @y_coordinate = @y_coordinate - 1
     end
+
+    def control
+    end
+
+    #Need to determine size of Mars
+    puts "What is your max x coordinate? - Enter number greater than 0" #Q1
+    max_x_coordinate = gets.chomp.to_i
+    puts "What is your max y coordinate? - Enter number greater than 0" #Q2
+    max_y_coordinate = gets.chomp.to_i
+
+    # Need to determine current position of rover
+    puts "What is your current x coordinate? - Enter number greater than 0" #Q3
+    current_x_coordinate = gets.chomp.to_i
+    puts "What is your current y coordinate? - Enter number greater than 0" #Q4
+    max_y_coordinate = gets.chomp.to_i
+
+    # Need to ask for user command R, L, M
+    puts "What do you want to do?:" # Q5
+    puts "Enter R for Turn Right, Enter L for Turn Left, Enter M for Move"
+    command = gets.chomp
 end`
