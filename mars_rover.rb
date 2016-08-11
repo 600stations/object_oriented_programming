8ad000ae4e563ab29ec076d169cc22973b90ea88
ChrisP <600steps@gmail.com>
2016-08-11 16:34:04 -0400

    first attempt at turn methods

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,25 +1,25 @@
 class Rover
   attr_accessor :x_coordinate, :y_coordinate, :direction
 
-def initialize(x_coordinate, y_coordinate,direction)# is this method needed if I use attr_accessor
+def initialize(x_coordinate, y_coordinate,direction)
   @x_coordinate = x_coordinate
   @y_coordinate = y_coordinate
   @direction = direction
 end
-end
-# pseudo code for right_turn methods and move
-def right_turn
+
+# 
+def right_turn(direction)#Not sure how to input
 case
-when N then new direction becomes E #how do I input starting N,E,W,S?
-when E then new direction becomes S
-when W then new direction becomes N
-when S then new direction becomes W
+  when "N" then @direction "E" #how do I input starting N,E,W,S?
+  when "E" then @direction "S"
+  when "W" then @direction "N"
+  when "S" then @direction "W"
 end
-# pseudo code for left_turn methods and move
-def left_turn
+#
+def left_turn(direction)#Not sure how to input
 case
-when N then new direction becomes W #how do I input starting N,E,W,S?
-when E then new direction becomes N
-when W then new direction becomes S
-when S then new direction becomes E
+  when "N" then new @direction "W" #how do I input starting N,E,W,S?
+  when "E" then new @direction "N"
+  when "W" then new @direction "S"
+  when "S" then new @direction "E"
 end
