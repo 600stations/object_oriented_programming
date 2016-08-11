9cc7d38e0f98ac67f632181b25cdfe449726edbf
ChrisP <600steps@gmail.com>
2016-08-11 16:43:57 -0400

    additions to move method

--- mars_rover.rb
+++ mars_rover.rb
@@ -7,19 +7,29 @@ def initialize(x_coordinate, y_coordinate,direction)
   @direction = direction
 end
 
-# 
-def right_turn(direction)#Not sure how to input
-case
-  when "N" then @direction "E" #how do I input starting N,E,W,S?
-  when "E" then @direction "S"
-  when "W" then @direction "N"
-  when "S" then @direction "W"
-end
-#
-def left_turn(direction)#Not sure how to input
-case
-  when "N" then new @direction "W" #how do I input starting N,E,W,S?
-  when "E" then new @direction "N"
-  when "W" then new @direction "S"
-  when "S" then new @direction "E"
-end
+# method to turn rover right
+def right_turn(e_direction)#Not sure how to input
+  case
+    when "N" then @direction "E" #how do I input starting N,E,W,S?
+    when "E" then @direction "S"
+    when "W" then @direction "N"
+    when "S" then @direction "W"
+  end
+# method to turn rover left
+def left_turn(e_direction)#Not sure how to input
+  case
+    when "N" then new @direction "W" #how do I input starting N,E,W,S?
+    when "E" then new @direction "N"
+    when "W" then new @direction "S"
+    when "S" then new @direction "E"
+  end
+  #
+  # method to move rover-pseudo code
+  def move(??) # pass in direction?
+    case
+      when "N" then @y_coordinate = @y_coordinate + 1
+      when "E" then @x_coordinate = @x_coordinate + 1
+      when "W" then @x_coordinate = @x_coordinate - 1
+      when "S" then @y_coordinate = @y_coordinate - 1
+    end
+end`
